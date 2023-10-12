f6b6558b37589f2ad5942c90203c9583157394d1
hudaputrasantosa <20104087@ittelkom-pwt.ac.id>
2023-10-12 23:32:40 +0700

    [add] controller for user

---
+++ controllers/userController.js
@@ -0,0 +1,80 @@
+const Computer = require("../models").Computer;
+const User = require("../models").User;
+
+module.exports = {
+  list(req, res) {
+    return User.findAll({
+      include: [],
+      order: [["createdAt", "DESC"]],
+    })
+      .then((users) => res.status(200).send(users))
+      .catch((error) => res.status(400).send(error));
+  },
+
+  getById(req, res) {
+    return User.findByPk(req.params.id, {
+      include: [
+        {
+          model: Computer,
+          as: "computer",
+        },
+      ],
+    })
+      .then((user) => {
+        if (!user) {
+          return res.status(400).send({ message: "User not found" });
+        }
+        return res.status(200).send(user);
+      })
+      .catch((error) => res.status(400).send(error));
+  },
+
+  add(req, res) {
+    return User.create({
+      id: req.body.id,
+      name: req.body.name,
+      nik: req.body.nik,
+      department_id: req.body.department_id,
+    })
+      .then((user) => res.status(201).send(user))
+      .then((error) => res.status(400).send(error));
+  },
+
+  update(req, res) {
+    return User.findByPk(req.params.id)
+      .then((user) => {
+        if (!user) {
+          res.status(404).send({
+            message: "User not found",
+          });
+        }
+        return user
+          .update({
+            name: req.body.name || user.name,
+            nik: req.body.nik || user.nik,
+            department_id: req.body.department_id || user.department_id,
+          })
+          .then(() => res.status(200).send(user))
+          .catch((error) => res.status(400).send(error));
+      })
+      .catch((error) => res.status(400).send(error));
+  },
+
+  delete(req, res) {
+    return User.findByPk(req.params.id).then((user) => {
+      if (!user) {
+        res.status(404).send({
+          message: "User not found",
+        });
+      }
+      return user
+        .destroy()
+        .then(() =>
+          res.status(204).send({
+            message: "Success delete",
+          })
+        )
+        .catch((error) => res.status(400).send(error));
+    });
+  },
+};
