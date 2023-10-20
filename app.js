cde02d91b1bfaf72f152ea1ac88206b0ff2be4af
hudaputrasantosa <20104087@ittelkom-pwt.ac.id>
2023-10-20 10:39:50 +0700

    [update] change root route

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const allRouters = require("./routes/index.js");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.use("/api", allRouters);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on http://localhost:${port}`);
 });
